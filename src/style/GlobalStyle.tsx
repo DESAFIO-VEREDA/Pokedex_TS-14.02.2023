eb5881f07ea368be1e818e5898033a82fefc313b
guicoelhodev <gs.coelho@outlook.com>
2023-02-14 14:41:17 -0300

    feat: add initial setup environment

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  width:100%;
+  background-color: #888;
+
+  * {
+    box-sizing: border-box;
+    padding:0;
+    margin:0;
+  }
+`
